2b1c5af694205b77efba2760a3e19f6ba3f10c2f
Laurine <deberdt.laurine@gmail.com>
2026-01-20 12:34:15 +0100

    Finalisation vue home

--- templates/home.php
+++ templates/home.php
@@ -5,25 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page d'accueil</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="/public/style.scss">
+    <link rel="stylesheet" href="../public/style.css">
 </head>
 <header>
-    <nav>
+    <nav class="navbar my-3 mx-5">
         <div class="container-fluid">
             <a class="navbar-brand" href="/">Touche pas au klaxon</a>
-            <button class="bg-dark">
-                <a href="/login">Connexion</a>
-            </button>
+            <a href="/login" class="btn btn-dark">Connexion</a>
+            
         </div>
     </nav>
 </header>
 <body>
-    <main>
-        <div><p>Pour obtenir plus d'informations sur un trajet, veuillez vous connecter</p></div>
+    <main class="mx-5">
+        <div><p class="dark-color info-text py-2">Pour obtenir plus d'informations sur un trajet, veuillez vous connecter</p></div>
 
-        <table class="table table-striped">
-            <thead>
-                <tr>
+        <table class="table table-striped table-bordered">
+            <thead class="text-center">
+                <tr class="dark-row">
                     <th>Départ</th>
                     <th>Date</th>
                     <th>Heure</th>
@@ -31,6 +30,7 @@
                     <th>Date</th>
                     <th>Heure</th>
                     <th>Places</th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>
@@ -42,26 +42,10 @@
                     <th></th>
                     <th></th>
                     <th></th>
-                </tr>
-                <tr>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
                     <th></th>
                 </tr>
                 <tr>
                     <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                </tr>
-                <tr>
                     <th></th>
                     <th></th>
                     <th></th>
@@ -78,14 +62,6 @@
                     <th></th>
                     <th></th>
                     <th></th>
-                </tr>
-                <tr>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                    <th></th>
                     <th></th>
                 </tr>
                 <tr>
@@ -96,8 +72,40 @@
                     <th></th>
                     <th></th>
                     <th></th>
+                    <th></th>
                 </tr>
                 <tr>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+                <tr>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+                <tr>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+                <tr>
+                    <th></th>
                     <th></th>
                     <th></th>
                     <th></th>
@@ -110,7 +118,7 @@
         </table>
     </main>
 </body>
-<footer>
+<footer class="d-flex justify-content-center">
     <div><p>&copy; 2026 - MVC PHP</p></div>
 </footer>
 </html>
