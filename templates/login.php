aae517d29771a042d18c8c28ebda80501877f1e7
Laurine <deberdt.laurine@gmail.com>
2026-01-26 15:04:10 +0100

    Template login

---
+++ templates/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire de connexion</title>
+</head>
+<body>
+    <main>
+        <div class="container">
+            <form action="" method="post" class="formulaire">
+                <h1>Connexion</h1>
+                <label for="">Nom d'utilisateur</label>
+                <input type="text" name="" id="">
+
+                <label for="">Mot de passe</label>
+                <input type="password" name="" id="">
+            </form>
+
+        </div>
+    </main>
+</body>
+</html>
