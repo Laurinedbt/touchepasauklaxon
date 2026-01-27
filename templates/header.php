0e1fbee60c8613230d641bc087b4628961569971
Laurine <deberdt.laurine@gmail.com>
2026-01-27 11:58:58 +0100

    Création header.php

---
+++ templates/header.php
@@ -0,0 +1,8 @@
+<header>
+    <nav class="navbar my-3 mx-5">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">Touche pas au klaxon</a>
+            <a href="/login" class="btn btn-dark">Connexion</a>   
+        </div>
+    </nav>
+</header>
